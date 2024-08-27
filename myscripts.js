b0de5f9c31fe82842202868ea372281644a72b96
thegreatestnathan <nathangolstein@gmail.com>
2024-08-27 15:04:48 +0200

    made a fun, interactive website around it

--- myscripts.js
+++ myscripts.js
@@ -13,43 +13,75 @@ function getHumanChoice () {
     return getHumanChoice ();
 }
 
-function playRound() {
+function playRound(humanChoice) {
     console.log("");
-    humanChoice = getHumanChoice();
+    /*    humanChoice = getHumanChoice(); */
     console.log("You chose " + humanChoice);
     computerChoice = getComputerChoice();
     console.log("The man behind your screen chose " + computerChoice);
-
+    /* in case of ties */
     if (humanChoice === computerChoice) {
-        console.log("You both picked the same! It's a tie :(");
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "rock")
-    ) {
-        console.log("You won this round!");
-        humanScore++;
-    } else {
-        console.log("Darn you suck");
-        computerScore++;
+        message = `You and the computer both chose <b>${humanChoice}</b>! It's a tie :(`;
+        announcement.innerHTML = message;     
     }
-    console.log(humanScore + " - " +computerScore)
+    /*in case of non-ties*/
+    else {
+        displayChoice = `You chose <b>${humanChoice}</b> whilst the computer chose <b>${computerChoice}</b>! `;
+        if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            message = "You won this round!";
+            humanScore++;
+        } else {
+            message = "Darn you suck";
+            computerScore++;
+        }
+        console.log(humanScore + " - " +computerScore)
+        announcement.innerHTML = displayChoice + message;}
+    score.textContent = humanScore + " - " + computerScore + "!";
+    if (humanScore == 5) {humanwin(humanScore, computerScore)}
+    if (computerScore == 5) {computerwin(humanScore, computerScore)}}
 
-}
 let humanScore = 0;
 let computerScore = 0;
 
+/*const scissors = document.querySelector("scissors");
+const rock = document.querySelector("rock");
+const paper = document.querySelector("paper");*/
 
-function playGame() {
+let buttons = document.querySelectorAll("#buttons button");
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    if (humanScore == computerScore) {console.log("It's a tie!")}
-    if (humanScore > computerScore) {console.log("You won!!!")}
-    if (humanScore < computerScore) {console.log("no comments...")}
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        console.log("button clicked");
+        let playerChoice = event.target.id;
+        console.log(playerChoice);
+        playRound(playerChoice);
+    });
+});
+
+
+const score = document.getElementById("score");
+const announcement = document.getElementById("announcement")
+const baby = document.getElementById("baby");
+
+winner = document.getElementById("winner");
+function humanwin(humanScore, computerScore) {
+    let message = `Good job! You beat the machine <b>${humanScore} - ${computerScore}!</b>`;
+    score.remove();
+    announcement.remove();
+    winner.innerHTML = message;
+    buttons.forEach(button => button.remove());
+    baby.src = 'images/win.jpg'
 }
 
-playGame();
+function computerwin(humanScore, computerScore) {
+    let message = `Bruh!! The machine beat you <b>${humanScore} - ${computerScore}!</b>`;
+    score.remove();
+    announcement.remove();
+    winner.innerHTML = message;
+    buttons.forEach(button => button.remove());
+    baby.src = 'images/loss.gif'
+}
